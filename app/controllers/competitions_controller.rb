93b0ce4cd6050dc203287733bc01cb2039dc5a66
Steven Barnes <slbccfl@gmail.com>
2016-09-26 12:44:07 -0400

    finally have a complete set of actions for competitions model

--- app/controllers/competitions_controller.rb
+++ app/controllers/competitions_controller.rb
@@ -3,18 +3,55 @@ class CompetitionsController < ApplicationController
 		@competitions = Competition.all
 	end
 	def new
+		@competition = Competition.new
+	end
+	def create
+	# Create a new competition from the parameters passed in the form
+		competition = Competition.new(
+			title: params[:competition][:title],
+			eventDatetime: params[:competition][:eventDatetime]
+		)
+
+		if competition.save
+			redirect_to "/competitions/#{competition.id}"
+		else
+			@competition = competition
+
+			render "new"
+		end
 	end
 	def show
-		competition = Competition.find_by(id: params[:id])
+		@competition = Competition.find_by(id: params[:id])
 
-		if competition
-			render show
+		if @competition
+			render :show
+		else
+		 	render status: 404, json: { error: "Competition #{params[:id]} not found" }
+		end
+	end
+	def edit
+		@competition = Competition.find_by(id: params[:id])
+
+		if @competition
+			render :edit
 		else
 		 	render status: 404, json: { error: "Competition #{params[:id]} not found" }
 		end
 	end
 	def update
+		competition = Competition.find_by(id: params[:id])
+
+		if competition.update(title: params[:competition][:title],
+				eventDatetime: params[:competition][:eventDatetime])
+			redirect_to competitions_path
+		else
+			render 'edit'
+		end
 	end
-	def destroy
+
+	def destroy as: 'destroy_competition'
+		competition = Competition.find_by(id: params[:id])
+		competition.destroy
+		redirect_to competitions_path
 	end
 end
