24e187a380c8e521aae0224cdbcee72ec1538994
Steven Barnes <slbccfl@gmail.com>
2016-10-21 14:12:08 -0400

    Good progress on fixing the team assignment bug and getting that functionality into place

--- app/controllers/team_assignments_controller.rb
+++ app/controllers/team_assignments_controller.rb
@@ -1,6 +1,11 @@
 class TeamAssignmentsController < ApplicationController
 	def index
-		@team = Team.find_by(id: params[:id])
-		@team_members = TeamMember.where(team_id: params[:id])
+		@team = Team.find_by(id: params[:team_id])
+		@team_members = TeamMember.where(team_id: params[:team_id])
+	end
+	def new
+		@team = Team.find_by(id: params[:team_id])
+		@team_members = TeamMember.where(team_id: params[:team_id])
+		@all_team_members = TeamMember.all
 	end
 end
