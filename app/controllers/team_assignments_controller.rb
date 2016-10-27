5d99ae5ba4ca5372c8c593d24ba3106d744fb23e
Steven Barnes <slbccfl@gmail.com>
2016-10-27 09:02:13 -0400

    after adding elements dual list plugin elements but before code changes to implement

--- app/controllers/team_assignments_controller.rb
+++ app/controllers/team_assignments_controller.rb
@@ -1,11 +1,11 @@
 class TeamAssignmentsController < ApplicationController
 	def index
 		@team = Team.find_by(id: params[:team_id])
-		@team_members = TeamMember.where(team_id: params[:team_id])
+		@team_members = TeamMember.where(team_id: params[:team_id]).order(lastName: :asc)
 	end
 	def new
 		@team = Team.find_by(id: params[:team_id])
-		@team_members = TeamMember.where(team_id: params[:team_id])
-		@all_team_members = TeamMember.all
+		@team_members = TeamMember.where(team_id: params[:team_id]).order(lastName: :asc)
+		@all_team_members = TeamMember.all.order(lastName: :asc)
 	end
 end
