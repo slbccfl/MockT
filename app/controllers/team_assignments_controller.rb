7b8bef720e628d44b43a6bb91dcc5230adad2365
Steven Barnes <slbccfl@gmail.com>
2016-10-28 15:23:39 -0400

    progress on implementing duallist for team_assignments

--- app/controllers/team_assignments_controller.rb
+++ app/controllers/team_assignments_controller.rb
@@ -8,4 +8,21 @@ class TeamAssignmentsController < ApplicationController
 		@team_members = TeamMember.where(team_id: params[:team_id]).order(lastName: :asc)
 		@all_team_members = TeamMember.all.order(lastName: :asc)
 	end
+	def create
+		puts 'create action found in team_assignments_controller.rb'
+		
+		params[:team_assigns][].each do | team_member_id |
+			if TeamAssignment.find_by(team_id: params[:team_id], team_member_id: team_member_id) == nil 
+				team_assignment = TeamAssignments.new(
+					:team_id => params[:team_id],
+					:team_member_id => team_member_id
+				)
+				if !(team_assignment.save!)
+					render status: 404, json: { error: "Could not create #{params[:id]} team_member" }
+				end
+			end
+		end
+
+		redirect_to edit_team_path(params[:team_id])
+	end
 end
