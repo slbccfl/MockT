e1e2bae0d7ce258b78e70ba101be329c9c9b2d04
Steven Barnes <slbccfl@gmail.com>
2016-10-31 10:30:24 -0400

    Victory is mine. Finally got duallistbox working for team assignments.

--- app/controllers/team_assignments_controller.rb
+++ app/controllers/team_assignments_controller.rb
@@ -5,15 +5,19 @@ class TeamAssignmentsController < ApplicationController
 	end
 	def new
 		@team = Team.find_by(id: params[:team_id])
-		@team_members = TeamMember.where(team_id: params[:team_id]).order(lastName: :asc)
+		team_assignments = TeamAssignment.where(team_id: params[:team_id])
+		@team_member_ids = team_assignments.map{ |team_assignment| team_assignment.team_member_id}
 		@all_team_members = TeamMember.all.order(lastName: :asc)
 	end
 	def create
 		puts 'create action found in team_assignments_controller.rb'
 		
-		params[:team_assigns][].each do | team_member_id |
+		TeamAssignment.where(team_id: params[:team_id]).destroy_all
+
+
+		params[:team_assigns].each do | team_member_id |
 			if TeamAssignment.find_by(team_id: params[:team_id], team_member_id: team_member_id) == nil 
-				team_assignment = TeamAssignments.new(
+				team_assignment = TeamAssignment.new(
 					:team_id => params[:team_id],
 					:team_member_id => team_member_id
 				)
